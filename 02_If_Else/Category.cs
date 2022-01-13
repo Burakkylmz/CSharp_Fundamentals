3f4d6affb5fb600d1168215934e128aa1b8e4388
Burak YILMAZ <burakk.ylmaz@outlook.com>
2022-01-13 13:42:49 +0300

    fdsfds

--- 02_If_Else/Category.cs
+++ 02_If_Else/Category.cs
@@ -9,5 +9,8 @@ namespace _02_If_Else
     internal class Category
     {
         public int MyProperty { get; set; }
+
+        public int Id { get; set; }
+        public DateTime CreateDate { get; set; }
     }
 }
