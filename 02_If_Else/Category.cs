62bb82a1b73fe5e5c4047345cb9f51b82b89d07b
Burak YILMAZ <burakk.ylmaz@outlook.com>
2022-01-13 13:38:38 +0300

    Category sınıfı eklendi

---
+++ 02_If_Else/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_If_Else
+{
+    internal class Category
+    {
+        public int MyProperty { get; set; }
+    }
+}
